d18a624067f151a64f3a5d639abec0af7859fdda
SamHeutmaker <samheutmaker@gmail.com>
2016-02-01 12:12:41 -0800

    Fixed routing

--- www/app/app.js
+++ www/app/app.js
@@ -1,28 +1,30 @@
 angular.module('notify', ['ngRoute', 'MainController', 'MainService'])
 
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+.config(['$routeProvider', '$locationProvider', function($routeProvider,
+	$locationProvider) {
 
 	$routeProvider
 
-	.when('/', {
+		.when('/', {
 		templateUrl: 'templates/home.html',
 		controller: 'HomeController'
 	})
 
 	.when('/login', {
-		templateUrl: 'templates/login.html',
-		controller: 'LoginController'
-	})
-	.when('/register', {
-		templateUrl: 'templates/register.html',
-		controller: 'RegisterController'
-	})
-	.when('/account', {
-		templateUrl: 'templates/account.html',
-		controller: 'AccountController'
-	})
+			templateUrl: 'templates/login.html',
+			controller: 'LoginController'
+		})
+		.when('/register', {
+			templateUrl: 'templates/register.html',
+			controller: 'RegisterController'
+		})
+		.when('/account', {
+			templateUrl: 'templates/account.html',
+			controller: 'AccountController'
+		})
 
-	.otherwise({redirectTo:'/'});
+	.otherwise({
+		redirectTo: '/'
+	});
 
-    $locationProvider.html5Mode(true);
 }])
