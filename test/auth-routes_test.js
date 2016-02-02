034bd147aa9a0c8b5fddb4c9af9d4dd519801188
Ardian Ajvazi <ardian_46@hotmail.com>
2016-02-02 15:37:37 -0800

    test for new User

--- test/auth-routes_test.js
+++ test/auth-routes_test.js
@@ -4,32 +4,30 @@ chai.use(chaiHTTP);
 var mongoose = require('mongoose');
 var expect = chai.expect;
 
+process.env.MONGO_URI = 'mongodb://notifyadmin:codefellows401@apollo.modulusmongo.net:27017/pY3miror';
+
 const server = require(__dirname + '/../server.js');
 const User = require(__dirname + '/../models/user.js');
-process.env.MONGOLAB_URI = 'mongodb://notifyadmin:codefellows401@apollo.modulusmongo.net:27017/G8ynygim'
 var baseUri = 'localhost:3000';
 
 describe('the authorization route', () => {
   after((done) => {
-    mongoose.connection.db.dropDatabase(() => {
+    User.remove({}, function(err) {
       done();
     });
   });
   // beforeEach((done) => {
-  //   User.create({authentication: [{ email: "notify@fellows.com", password: 'password'}]}, (err, data) => {
+  //   User.create({authentication: { email: "notify@fellows.com", password: 'password'}}, (err, data) => {
   //     console.log(data);
   //     this.testUser = data;
   //     done();
   //   });
   // })
-  // afterEach((done) => {
-  //   User.remove({});
-  //   done();
-  // })
+
   it('should create a new user with a POST requests', (done) => {
     chai.request(baseUri)
       .post('/auth/register')
-      .send({"authentication": {"email":"notifyv5@fellows.com", "password":"password1"}})
+      .send({"email":"notifyv11@fellows.com", "password":"password2"})
       .end((err, res) => {
         expect(err).to.eql(null);
         expect(res).to.have.status(200);
